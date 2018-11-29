9c079f21ad9d001b8254c72a4201a598e2787216
carpenter-js <carpenterbarrett@gmail.com>
2018-11-28 18:21:10 -0700

    deploy

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error' });
   }
-});
+});    
 
 if (require.main === module) {
   // Connect to DB and Listen for incoming connections
